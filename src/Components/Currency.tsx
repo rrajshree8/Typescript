3bb71dd7cab561b3c8ae28a8b001da64ee2d4503
rrajshree8 <rrajshreesingh28@gmail.com>
2024-03-15 17:10:02 +0530

    final touch

--- src/Components/Currency.tsx
+++ src/Components/Currency.tsx
@@ -1,47 +1,47 @@
-import { Box, Wrap } from '@chakra-ui/react'
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { HTTP_URL } from '../utils'
+
+import { Box, Wrap } from "@chakra-ui/react";
+import { CheckCircleIcon } from "@chakra-ui/icons";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { HTTP_URL } from "../utils";
 
 const Currency = () => {
+  const [currencies, setCurrencies] = useState([]);
+  const [selectedCurrency, setSelectedCurrency] = useState(null);
 
-    const [currencies,setCurrencies]=useState([])
+  useEffect(() => {
+    axios
+      .get(`${HTTP_URL}/public/fiat-currency`)
+      .then(function (response) {
+        setCurrencies(response.data.data.currencies);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
 
-
-    useEffect(()=>{
-
-        axios.get(`${HTTP_URL}/public/fiat-currency`)
-  .then(function (response) {
-    setCurrencies(response.data.data.currencies)
-
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-
-    },[])
+  const handleCurrencySelect = (index) => {
+    setSelectedCurrency(index);
+  };
 
   return (
-    <Wrap gap={'10px'} flexBasis={'1'}>
- 
+    <Wrap gap={"10px"} flexBasis={"1"}>
+      {currencies.map((currency, idx) => (
+        <Box
+          key={idx}
+          as="button"
+          borderRadius="md"
+          _hover={{ background: "grey", color: "white" }}
+          px={4}
+          h={8}
+          onClick={() => handleCurrencySelect(idx)}
+        >
+          {currency.name} - {currency.symbol}{" "}
+          {selectedCurrency === idx && <CheckCircleIcon color="black.500" />}
+        </Box>
+      ))}
+    </Wrap>
+  );
+};
 
-{currencies && currencies?.map((item:any,idx)=>{
-    return(
-<Box key={idx} as='button' borderRadius='md'  _hover={{ background: 'grey', color:'white' }}  px={4} h={8}>
-  {item.name} - {item.symbol}
-</Box>
-    )
-})}
-
-
-
-
-
-
-   </Wrap>
-  )
-}
-
-
-export default Currency
+export default Currency;
--- src/Components/Language.tsx
+++ src/Components/Language.tsx
@@ -1,45 +1,50 @@
-import { Box, Wrap } from '@chakra-ui/react'
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { HTTP_URL } from '../utils'
+import { Box, Wrap } from "@chakra-ui/react";
+import { CheckCircleIcon } from "@chakra-ui/icons";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { HTTP_URL } from "../utils";
 
 const Language = () => {
+  const [lang, setLang] = useState([]);
+  const [selectedLang, setSelectedLang] = useState(null);
 
-    const [lang,setLang]=useState([])
+  useEffect(() => {
+    axios
+      .get(`${HTTP_URL}/public/lang`)
+      .then(function (response) {
+        setLang(response.data.data);
+        // handle success
+        console.log(response.data.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }, []);
 
-
-    useEffect(()=>{
-
-axios.get(`${HTTP_URL}/public/lang`)
-  .then(function (response) {
-    setLang(response.data.data)
-    // handle success
-    console.log(response.data.data);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-
-    },[])
-
+  const handleLangSelect = (index) => {
+    setSelectedLang(index);
+  };
 
   return (
-    <Wrap gap={'10px'} flexBasis={'1'}>
-   
-   {lang && lang?.map((item:any,idx)=>{
-    return(
-<Box key={idx} as='button' borderRadius='md'  _hover={{ background: 'grey', color:'white' }}  px={4} h={8}>
-{item.lang}
-</Box>
-    )
-   })}
+    <Wrap gap={"10px"} flexBasis={"1"}>
+      {lang &&
+        lang.map((item, idx) => (
+          <Box
+            key={idx}
+            as="button"
+            borderRadius="md"
+            _hover={{ background: "grey", color: "white" }}
+            px={4}
+            h={8}
+            onClick={() => handleLangSelect(idx)}
+          >
+            {item.lang}{" "}
+            {selectedLang === idx && <CheckCircleIcon color="black.500" />}
+          </Box>
+        ))}
+    </Wrap>
+  );
+};
 
-
-
-
-   </Wrap>
-  )
-}
-
-export default Language
+export default Language;
